0cd792a7ccfc1f22404e2fd267b74a41f233134c
haker500 <grzees@gmail.com>
2017-06-25 02:03:45 +0200

    day27

---
+++ day27/day27.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class day27
+{
+    public static void main(String[] args)
+    {
+        System.out.println("5");
+        System.out.println("4 3");
+        System.out.println("-3 0 2 4");
+        System.out.println("5 2");
+        System.out.println("-1 0 0 1 2");
+        System.out.println("6 4");
+        System.out.println("-2 0 2 3 4 5");
+        System.out.println("7 5");
+        System.out.println("-3 -2 0 0 0 2 3");
+        System.out.println("8 4");
+        System.out.println("-1 -1 0 2 3 3 4 4");
+    }
+}
