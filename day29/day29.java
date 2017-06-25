1a93ac228413043252914ee1a0b9c573316f2736
haker500 <grzees@gmail.com>
2017-06-25 02:04:27 +0200

    day29

---
+++ day29/day29.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class day29
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int max = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    int result = i & j;
+
+                    if (result > max && result < k)
+                    {
+                        max = result;
+                    }
+                }
+            }
+
+            System.out.println(max);
+        }
+    }
+}
